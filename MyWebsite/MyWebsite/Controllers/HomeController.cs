fe804abba2818d5370a189b9509a573ddb8bddc7
LAPTOP-4L8OU9T6\Mahmud <mhkbgd@Gmail.com>
2017-09-06 21:22:10 +0600

    Initial commit of my website codes

---
+++ MyWebsite/MyWebsite/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyWebsite.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            string Name = Request.Form["Name"];
+            string Email = Request.Form["Email"];
+            string Message = Request.Form["Message"];
+            if (Email != null) {
+                TempData["Success"] = "Thanks for contacting will be get back to you soon";
+
+            }
+
+            return View();
+        }
+
+       
+
+        
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
